1408d265056d2b68e23a1e21b76da97756a29ff6
tattiii8 <t.yoshimura@lesure.net>
2026-08-31 22:12:05 +0900

    ..

--- modules/gateway/main.tf
+++ modules/gateway/main.tf
@@ -1,5 +1,5 @@
 resource "nomad_variable" "gateway" {
-  path = "nomad/jobs/flaubert-gateway"
+  path = "nomad/jobs/spinoza-gateway"
 
   items = {
     CLOUDFLARE_TUNNEL_TOKEN = var.cloudflare_tunnel_token
@@ -8,8 +8,8 @@ resource "nomad_variable" "gateway" {
   }
 }
 
-resource "nomad_job" "flaubert_gateway" {
-  jobspec = templatefile("${path.module}/templates/flaubert-gateway.nomad.hcl.tpl", {
+resource "nomad_job" "spinoza_gateway" {
+  jobspec = templatefile("${path.module}/templates/spinoza-gateway.nomad.hcl.tpl", {
     datacenter   = var.datacenter
     ecr_registry = var.ecr_registry
   })
